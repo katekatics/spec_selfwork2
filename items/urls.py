191fd56d5aad0640706300d62a039c63047835d2
katekatics <E.Astrakhantseva@x5.ru>
2020-12-03 19:53:12 +0300

    urls fixeed

--- items/urls.py
+++ items/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", ItemsListView.as_view(), name="list_items"),
     path("new/", ItemCreateView.as_view(), name="new_item"),
     path("<int:pk>/edit/", ItemEditView.as_view(), name="edit_item"),
-    path("<int:pk>/", ItemDetailView.as_view(), name="detail_item"),
+    path("<int:pk>/detail/", ItemDetailView.as_view(), name="detail_item"),
     path("<int:pk>/delete/", ItemDeleteView.as_view(), name="delete_item")
 ]
